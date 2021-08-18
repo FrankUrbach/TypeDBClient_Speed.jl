84f3a59686acc541275d9869d7414f201964f85b
Documenter.jl <documenter@juliadocs.github.io>
2021-08-18 22:39:31 +0000

    build based on 5c033bb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TypeDBClient_Speed","category":"page"},{"location":"#TypeDBClient_Speed","page":"Home","title":"TypeDBClient_Speed","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TypeDBClient_Speed.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TypeDBClient_Speed]","category":"page"}]
+}
